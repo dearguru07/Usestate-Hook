8b312655f1c38154e8eb7bdb6b8a41d74d9afa2b
dearguru07 <pangalaguruyadav101@gmail.com>
2024-10-24 14:37:52 +0530

    'st'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const StateHook=()=>{
+  const [a,seta]=useState(0)
+  return <div>
+    <center>
+      <h1>--------Usestate Hook--------</h1>
+    <h2>Counter app</h2>
+    <h4>{a}</h4>
+    <button onClick={()=>{
+      seta(a+1)
+    }}>Increase</button>
+    <button onClick={()=>{
+      seta(a-1)
+    }}>Decrease</button>
+    </center>
+  </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<StateHook/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
